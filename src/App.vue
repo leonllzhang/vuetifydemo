10d35b54e931723a32d445e64803a8898076f94b
Leon LL Zhang <leon.ll.zhang@cn.pwc.com>
2019-05-24 15:21:29 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,346 @@
+
+<template>
+  <v-app id="inspire">
+    <v-navigation-drawer
+      class=""
+      v-model="drawer"
+      :clipped="$vuetify.breakpoint.lgAndUp"
+      fixed
+      app
+    >  
+
+<!-- mini menus -->
+ <v-toolbar flat class="transparent">
+        <v-list class="pa-0">
+          <v-list-tile avatar>
+            <v-list-tile-avatar>
+              <img src="https://randomuser.me/api/portraits/men/85.jpg">
+            </v-list-tile-avatar>
+  
+            <v-list-tile-content>
+              <v-list-tile-title>John Leider</v-list-tile-title>
+            </v-list-tile-content>
+  
+            <v-list-tile-action>
+              <v-btn
+                icon
+                @click.stop="mini = !mini"
+              >
+                <v-icon>chevron_left</v-icon>
+              </v-btn>
+            </v-list-tile-action>
+          </v-list-tile>
+        </v-list>
+      </v-toolbar>
+  
+      <v-list class="pt-0" dense>
+        <v-divider></v-divider>
+  
+        <v-list-tile
+          v-for="item in items"
+          :key="item.title"
+          @click=""
+        >
+          <v-list-tile-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-tile-action>
+  
+          <v-list-tile-content>
+            <v-list-tile-title>{{ item.text }}</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+
+    </v-navigation-drawer>
+
+<!-- Toolbar top -->
+
+
+    <v-toolbar
+      :clipped-left=false
+      color="blue darken-3"
+      dark
+      app
+      fixed
+    >
+      <v-toolbar-title style="width: 300px" class="ml-0 pl-3">
+        <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+        <span class="hidden-sm-and-down">Faserati</span>
+      </v-toolbar-title>
+      <v-btn icon>
+        <v-icon medium="" aria-label="dd">how_to_reg</v-icon>
+      </v-btn>
+      <v-btn icon>
+        <v-icon>apps</v-icon>
+      </v-btn>
+      <v-btn icon>
+        <v-icon>looks_one</v-icon>
+      </v-btn>
+      <v-btn icon>
+        <v-icon>looks_two</v-icon>
+      </v-btn>
+ <!-- <v-layout justify-space-around>
+   
+ </v-layout> -->
+      
+      <v-text-field
+        flat
+        solo-inverted
+        hide-details
+        append-icon="call"
+        label="Call"
+        :mask="mask"
+        class="hidden-sm-and-down"
+      ></v-text-field>
+      <!-- <v-spacer></v-spacer> -->
+      <v-btn icon>
+        <v-icon>chat</v-icon>
+      </v-btn>
+       <v-btn icon>
+        <v-icon>phone_forwarded</v-icon>
+      </v-btn>
+       <v-btn icon>
+        <v-icon>people</v-icon>
+      </v-btn>
+       <v-btn icon>
+        <v-icon>phone_missed</v-icon>
+      </v-btn>
+       <v-btn icon>
+        <v-icon>phone_callback</v-icon>
+      </v-btn>
+       <v-btn icon>
+        <v-icon>contact_phone</v-icon>
+      </v-btn>
+      <v-btn icon>
+        <v-icon>notifications</v-icon>
+      </v-btn>
+      <v-btn icon large>
+        <v-avatar size="32px" tile>
+          <img
+            src="https://cdn.vuetifyjs.com/images/logos/logo.svg"
+            alt="Vuetify"
+          >
+        </v-avatar>
+      </v-btn>
+      <template v-slot:extension>
+        <v-tabs
+          v-model="tab"
+          color="transparent"          
+          centered
+          icons-and-text
+        >
+          <v-tabs-slider color="yellow"></v-tabs-slider>
+
+          <v-tab v-for="item in tabs" :key="item.key">
+            {{item.text}}
+            <v-icon>{{item.icon}}</v-icon>
+          </v-tab>
+        </v-tabs>
+      </template>
+    </v-toolbar>
+    <v-content>
+      <v-container fluid fill-height>
+        <v-layout justify-center align-center>
+          <template>
+  <v-layout justify-center>
+    <v-flex xs12 sm6>
+     
+
+      <v-card>
+        <v-container
+          fluid
+          grid-list-md
+        >
+          <v-layout row wrap>
+            <v-flex
+              v-for="card in cards"
+              :key="card.title"
+              v-bind="{ [`xs${card.flex}`]: true }"
+            >
+              <v-card>
+                <v-img
+                  :src="card.src"
+                  height="200px"
+                >
+                  <v-container
+                    fill-height
+                    fluid
+                    pa-2
+                  >
+                    <v-layout fill-height>
+                      <v-flex xs12 align-end flexbox>
+                        <span class="headline white--text" v-text="card.title"></span>
+                      </v-flex>
+                    </v-layout>
+                  </v-container>
+                </v-img>
+<v-card-title>
+          <div>
+            <span class="grey--text"  v-text="card.title"></span><br>
+            <span v-text="card.text"></span><br>
+            <span v-text="card.text"></span>
+          </div>
+        </v-card-title>
+                <v-card-actions>
+                  <v-spacer></v-spacer>
+                  <v-btn icon>
+                    <v-icon>favorite</v-icon>
+                  </v-btn>
+                  <v-btn icon>
+                    <v-icon>bookmark</v-icon>
+                  </v-btn>
+                  <v-btn icon>
+                    <v-icon>share</v-icon>
+                  </v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-flex>
+          </v-layout>
+        </v-container>
+      </v-card>
+    </v-flex>
+  </v-layout>
+</template>
+        </v-layout>
+      </v-container>
+    </v-content>
+    <v-btn
+      fab
+      bottom
+      right
+      color="pink"
+      dark
+      fixed
+      @click="dialog = !dialog"
+    >
+      <v-icon>add</v-icon>
+    </v-btn>
+    <v-dialog v-model="dialog" width="800px">
+      <v-card>
+        <v-card-title
+          class="grey lighten-4 py-4 title"
+        >
+          Create contact
+        </v-card-title>
+        <v-container grid-list-sm class="pa-4">
+          <v-layout row wrap>
+            <v-flex xs12 align-center justify-space-between>
+              <v-layout align-center>
+                <v-avatar size="40px" class="mr-3">
+                  <img
+                    src="//ssl.gstatic.com/s2/oz/images/sge/grey_silhouette.png"
+                    alt=""
+                  >
+                </v-avatar>
+                <v-text-field
+                  placeholder="Name"
+                ></v-text-field>
+              </v-layout>
+            </v-flex>
+            <v-flex xs6>
+              <v-text-field
+                prepend-icon="business"
+                placeholder="Company"
+              ></v-text-field>
+            </v-flex>
+            <v-flex xs6>
+              <v-text-field
+                placeholder="Job title"
+              ></v-text-field>
+            </v-flex>
+            <v-flex xs12>
+              <v-text-field
+                prepend-icon="mail"
+                placeholder="Email"
+              ></v-text-field>
+            </v-flex>
+            <v-flex xs12>
+              <v-text-field
+                type="tel"
+                prepend-icon="phone"
+                placeholder="(000) 000 - 0000"
+                mask="phone"
+              ></v-text-field>
+            </v-flex>
+            <v-flex xs12>
+              <v-text-field
+                prepend-icon="notes"
+                placeholder="Notes"
+              ></v-text-field>
+            </v-flex>
+          </v-layout>
+        </v-container>
+        <v-card-actions>
+          <v-btn flat color="primary">More</v-btn>
+          <v-spacer></v-spacer>
+          <v-btn flat color="primary" @click="dialog = false">Cancel</v-btn>
+          <v-btn flat @click="dialog = false">Save</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    components:{
+      DataTable: "DataTable"
+    },
+    data: () => ({
+      dialog: false,
+      drawer: true,
+      mask:'phone',
+      items: [
+        { icon: 'notifications', text: '系统消息' },
+        { icon: 'history', text: 'Frequently contacted' },
+        { icon: 'content_copy', text: 'Duplicates' },
+        {
+          icon: 'keyboard_arrow_up',
+          'icon-alt': 'keyboard_arrow_down',
+          text: 'Labels',
+          model: true,
+          children: [
+            { icon: 'add', text: 'Create label' }
+          ]
+        },
+        {
+          icon: 'keyboard_arrow_up',
+          'icon-alt': 'keyboard_arrow_down',
+          text: 'More',
+          model: false,
+          children: [
+            { text: 'Import' },
+            { text: 'Export' },
+            { text: 'Print' },
+            { text: 'Undo changes' },
+            { text: 'Other contacts' }
+          ]
+        },
+        { icon: 'settings', text: 'Settings' },
+        { icon: 'chat_bubble', text: 'Send feedback' },
+        { icon: 'help', text: 'Help' },
+        { icon: 'phonelink', text: 'App downloads' },
+        { icon: 'keyboard', text: 'Go to the old version' }
+      ],
+      tabs:[
+        {key:'1',text:'首页',icon:'book'},
+        {key:'2',text:'首页',icon:'book'},
+        {key:'3',text:'首页',icon:'book'},
+        {key:'4',text:'首页',icon:'book'},
+        {key:'5',text:'首页',icon:'book'},
+        {key:'6',text:'首页',icon:'book'}
+
+      ],
+      mini:true,
+
+      cards: [
+        { title: 'Pre-fab homes', src: 'https://cdn.vuetifyjs.com/images/cards/house.jpg', flex: 12, text:'Whitsunday Island, Whitsunday Islands' },
+        { title: 'Favorite road trips', src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg', flex: 6,text:'Listen to your favorite artists and albums whenever and wherever, online and offline.' },
+        { title: 'Best airlines', src: 'https://cdn.vuetifyjs.com/images/cards/plane.jpg', flex: 6,text:'Ellie Goulding(2013)' }
+      ]
+    }),
+    props: {
+      source: String
+    }
+  }
+</script>
